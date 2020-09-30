234882221e7dbe20d94640b8fdc28cda641efc96
raffay-rafique <raffay-rafique@hotmail.com>
2020-09-30 19:14:24 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todos from './components/Todos';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+    
+  },
+  data(){
+    return{
+      todos:[{
+        id:1,
+        title:"Todo One",
+        completed:false
+      },
+      {
+        id:2,
+        title:"Todo 2",
+        completed:false
+      },
+      {
+        id:3,
+        title:"Todo 3",
+        completed:false
+      }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p></p>
+    </div>
+</template>
+
+<script>
+export default {
+name:"TodoItem"
+}
+</script>
+
+<style scoped>
+
+</style>
